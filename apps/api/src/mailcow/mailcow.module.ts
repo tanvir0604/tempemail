b6e8d237b125c5cd45fc369f17e4c2d593e44493
Md Shafkat Hussain Tanvir <tanvir0604@gmail.com>
2025-11-21 22:42:09 +0600

    create aliase

--- apps/api/src/mailcow/mailcow.module.ts
+++ apps/api/src/mailcow/mailcow.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MailcowController } from './mailcow.controller';
 import { MailcowService } from './mailcow.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [MailcowController],
-  providers: [MailcowService]
+  providers: [MailcowService],
 })
 export class MailcowModule {}
--- apps/api/src/mailcow/mailcow.service.ts
+++ apps/api/src/mailcow/mailcow.service.ts
@@ -1,4 +1,14 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MailcowService {}
+export class MailcowService {
+  constructor(private readonly httpService: HttpService) {}
+  async createNewAlias() {
+    const data = {
+      active: '1',
+      address: 'alias@domain.tld',
+      goto: 'destination@domain.tld',
+    };
+  }
+}
