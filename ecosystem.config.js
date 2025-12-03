a9aa5849f10b10e3481c0802aede947b44d69911
Md Shafkat Hussain Tanvir <tanvir0604@gmail.com>
2025-12-03 15:42:47 +0600

    blog production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,42 +1,51 @@
 const path = require('path');
 
 module.exports = {
-	apps: [
-		{
-			name: 'temp-email-web',
-			script: 'pnpm',
-			args: '--filter=web start',
-			cwd: path.resolve(__dirname, './apps/web'),
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-		{
-			name: 'temp-email-api',
-			script: 'pnpm',
-			args: '--filter=api start',
-			cwd: path.resolve(__dirname, './apps/api'),
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-		{
-			name: 'temp-email-mailcow',
-			script: 'pnpm',
-			args: '--filter=mailcow start',
-			cwd: path.resolve(__dirname, './apps/mailcow'),
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-		{
-			name: 'temp-email-settings',
-			script: 'pnpm',
-			args: '--filter=settings start',
-			cwd: path.resolve(__dirname, './apps/settings'),
-			env: {
-				NODE_ENV: 'production',
-			},
-		},
-	],
+    apps: [
+        {
+            name: 'temp-email-web',
+            script: 'pnpm',
+            args: '--filter=web start',
+            cwd: path.resolve(__dirname, './apps/web'),
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'temp-email-api',
+            script: 'pnpm',
+            args: '--filter=api start',
+            cwd: path.resolve(__dirname, './apps/api'),
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'temp-email-mailcow',
+            script: 'pnpm',
+            args: '--filter=mailcow start',
+            cwd: path.resolve(__dirname, './apps/mailcow'),
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'temp-email-settings',
+            script: 'pnpm',
+            args: '--filter=settings start',
+            cwd: path.resolve(__dirname, './apps/settings'),
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'temp-email-blog',
+            script: 'pnpm',
+            args: '--filter=blog start',
+            cwd: path.resolve(__dirname, './apps/blog'),
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
 };
